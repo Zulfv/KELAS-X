6ef887b822ffab938b31adda5c010c1bd399c4bc
Zulfv <auliaanjanizulfi@gmail.com>
2024-11-29 21:45:37 +0700

    Add files via upload

--- 2024-10-29/index.php
+++ 2024-10-29/index.php
@@ -1,40 +1,93 @@
 <?php
-$sekolah=["tk wanita","sdn sidokare 1","smpn 6 sidoarjo","smkn 2 buduran"]
+$sekolah = [
+    "TK AISSAH",
+    "SDN SIDOKARE 1",
+    "SMPN 6 SIDOARJO",
+    "SMKN 2 BUDURAN",
+];
 
-$sekolah=["tk"=>"tk waniata","sd"=>"sdn sidokare 1","smp"=>"smpn 6 sidoarjo","smk"=>"smkn 2 buduran"]
+$sekolahs = [
+    "TK"=>"TK AISSIYAH",
+    "SD"=>"SDN SIDOKARE 1",
+    "SMP"=>"SMPN 6 SIDOARJO",
+    "SMK"=>"SMKN 2 BUDURAN",
+    "PT"=>"SEKOLAH TINGGI INTELIJEN NEGARA",
+];
 
-$skills=["c++"=>"expert",]
+$skills = [
+    "C++"=>"Expert",
+    "HTML"=>"Newbie",
+    "CSS"=>"Newbie",
+    "PHP"=>"Intermediet",
+    "JavaScript"=>"Intermediet",
+];
 
-echo$sekolah[0];
-echo"<br>";
-echo$sekolah["tk"];
-echo"<br>";
-echo$sekolah[1];
-echo"<br>";
-echo$sekolah["sd"];
-echo"<br>";
+$identitas = [
+    "Nama"=>"ZULFI AULIA ANAJNI",
+    "Alamat"=>"KUTUK TENGA RT 8 RW 7",
+    "Email"=>"AULIAANJANIZULFI@GMAIL.COM",
+    "Facebook"=>"ZULFIII",
+    "Tiktok"=>"BUTTERFLY",
+    "Instagram"=>"ZLFY",
+];
 
+$hobby = [
+    "BERMAIN KUCING",
+    "BERPETUALANG",
+    "MUSIK",
+    "MANCING",
+    "BERENANG",
+];
 
-for($i = 0; $i<4 ; $i++){
-    echo $sekolah[$i];
-    echo"<br>";
+// echo $sekolah[0];
+// echo "<br>";
+// echo $sekolahs["TK"];
+// echo "<br>";
+// echo $sekolah[1];
+// echo "<br>";
+// echo $sekolahs["SD"];
+
+// echo "<br>";
+
+// //menampilkan secara keseluruhan
+
+// for ($i = 0; $i < 4;$i++){
+//     echo $sekolah[$i];
+//     echo "<br>";
+// }
+
+// echo "<br>";
+
+// foreach ($sekolah as $key) {
+//     echo $key;
+//     echo "<br>";
+// }
+
+// echo "<br>";
+
+// foreach ($sekolahs as $key => $value){
+//     echo $key;
+//     echo "=";
+//     echo $value;
+//     echo "<br>";
+// }
+
+// echo "<br>";
+
+// foreach ($skills as $key => $value) {
+//     echo $key;
+//     echo "=";
+//     echo $value;
+//     echo "<br>";
+// }
+
+if (isset($_GET["menu"])) {
+    $menu = $_GET["menu"];
+    echo $menu;
 }
 
-echo"<br>";
 
-foreach($sekolah as$key){
-    echo$key;
-    echo"<br>";
-}
 
-echo"<br>";
-
-foreach($sekolah as$key => $velue){
-    echo$key;
-    echo"=";
-    echo $velue;
-    echo"<br>";
-}
 ?>
 
 <!DOCTYPE html>
@@ -45,6 +98,87 @@ foreach($sekolah as$key => $velue){
     <title>Document</title>
 </head>
 <body>
-    <h2>RIWAYAT SEKOLAH</h2>
+    <hr>
+    <ul>
+        <li><a href="?menu=home">Home</a></li>
+        <li><a href="?menu=cv">CV</a></li>
+        <li><a href="?menu=Project">Project</a></li>
+        <li><a href="?menu=Contact">Contact</a></li>
+    </ul>
+    <h2>Identitas</h2>
+    <table border=1px>
+        <thead>
+            <th>Identitas</th>
+            <th>deskripsi</th>
+        </thead>
+        <tbody>
+            <?php
+            foreach ($identitas as $key => $value) {
+            ?>
+            <tr>
+            <td><?= $key ?></td>
+            <td><?= $value ?></td>
+            </tr>
+            <?php
+            }
+            ?>
+        </tbody>
+    </table>
+    <h2>Riwayat sekolah</h2>
+    <table border=1px>
+        <thead>
+            <tr>
+                <th>Jenjang</th>
+                <th>Nama Sekolah</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            foreach ($sekolahs as $key => $value){
+                echo "<tr>";
+                echo "<td>";
+                echo $key;
+                echo "</td>";
+                echo "<td>";
+                echo $value;
+                echo "</td>";
+                echo "</tr>";
+            }
+            ?>
+        </tbody>
+    </table>
+    <hr>
+    <h2>SKILLS</h2>
+    <table border=1px>
+        <thead>
+            <tr>
+                <th>Skill</th>
+                <th>Level</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            foreach ($skills as $key => $value) {
+            ?>
+            <tr>
+                <td><?= $key ?></td>
+                <td><?= $value ?></td>
+            </tr>
+            <?php
+            }
+            ?>
+        </tbody>
+    </table>
+    <hr>
+    <h2>HOBBY</h2>
+    <ul>
+        <?php
+        foreach ($hobby as $key) {
+        ?>
+        <li><?= $key ?></li>
+        <?php
+        }
+        ?>
+    </ul>
 </body>
 </html>
---
+++ 2024-10-8/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="warna-background container-fluid">
+        <div class="container">
+            <h1>Belajar bootstrap</h1>
+            <div class="navbar navbar-expand-md jarak">
+                <ul class="navbar-nav jarak">
+                    <li class="nav-item">Home</li>
+                    <li class="nav-item">Sejarah</li>
+                    <li class="nav-item">Jurusan</li>
+                    <li class="nav-item">ekstrakulikuler</li>
+                    <li class="nav-item">PPDB</li>
+                    <li class="nav-item">Contact</li>
+                </ul>
+                <div>
+                    <h1 class="text-danger">Logo</h1>
+                    <div>
+                        <button class="btn-primary">Kirim</button>
+                    </div>
+                </div>
+            </div>
+            <form class="" action="">
+                <div col-md-4>
+                    <label class="form-label" for="">Email</label>
+                    <input class="form-control" type="email" name="" id="">
+                </div>
+                <div col-md-4>
+                    <label class="form-label" for="">Password</label>
+                    <input class="form-control" type="password" name="" id="">
+                </div>
+                <div>
+                    <button class="btn btn-primary" type="submit">Kirim</button>
+                </div>
+            </form>
+        </div>
+    </div>
+    <div class="row">
+        <div class="d-md-flex gap-3 col-5-lg">
+            <input class="form-control" type="text">
+            <input class="form-control" type="email" name="" id="">
+            <input class="form-control" type="password" name="" id="">
+            <input class="form-control" type="time" name="" id="">
+            <input class="form-control" type="date" name="" id="">
+        </div>
+    </div>
+</body>
+</html>
